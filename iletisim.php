d18d0dbace56525ed4ba1ef3b21b4de2a602ef58
BAYRAKTAR <m.bayraktar01@hotmail.com>
2022-05-04 01:17:14 +0300

    up

--- iletisim.php
+++ iletisim.php
@@ -6,21 +6,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-	<link href="style.css" rel="stylesheet" type="text/css">
+	  <link href="style.css" rel="stylesheet" type="text/css">
 
 	  
     <title>Mustafa Bayraktar</title>
   </head>
-  <body>
- <nav class="navbar navbar-expand-lg navbar-light bg-a fixed-top">
-  <a class="navbar-brand yazi" href="#" style="font-size: 20px;">Mustafa Bayraktar</a>
+  <body background="C:\Users\WİN10\Desktop\JPG\haber-bandi_1353113517_1000x700_false.jpg">
+<header>
+ <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+  <a class="navbar-brand" href="#" style="font-size: 20px;">Mustafa Bayraktar</a>
   <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
-    <ul class="navbar-nav ">
+    <ul class="navbar-nav">
       <li class="nav-item active">
         <a class="nav-link" href="index.html">ANASAYFA</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="özgecmis.html">Özgeçmiş</a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="sehrim.html">Şehrim</a>
@@ -28,14 +26,14 @@
       <li class="nav-item">
         <a class="nav-link" href="iletisim.html">İletişim</a>
       </li>
-     
+    
       <li class="nav-item">
         <a class="nav-link" href="loginsayfasi.html">Giriş Yap</a>
-      </li>
+      </li>  
     </ul>
   </div>
 </nav>
-
+</header>
 <div class="container">
     
     </div>
