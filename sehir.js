4a333a62b2879002b3cc7ecd89e99cc33440ee6c
BAYRAKTAR <m.bayraktar01@hotmail.com>
2022-05-12 00:42:37 +0300

    update

---
+++ sehir.js
@@ -0,0 +1,20 @@
+const url='https://api.openweathermap.org/data/2.5/'
+const key='5419f3be03caa929488a3294fb9ab717'
+
+
+const getResut=()=>{
+    let query=`${url}weather?q=${Trabzon}&appid=${key}&units=metric&lang=tr`
+    fetch(query)
+    .then(weather=>{
+        return weather.json()
+    })
+    .then(displayResult)}
+
+    const displayResult=(result)=>
+    {
+    let temp = document.querySelector('.temp');
+    temp.innerHTML = `Sıcaklık : ${Math.round(result.main.temp)}°C`;
+    let desc = document.querySelector('.desc');
+    desc.innerHTML = `Hava ${result.weather[0].description}`;
+    
+    }
