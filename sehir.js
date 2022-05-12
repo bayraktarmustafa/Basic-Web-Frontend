f8aaad1da53fa1a7958ae1cd4f8c8abd459f461d
BAYRAKTAR <m.bayraktar01@hotmail.com>
2022-05-12 15:03:40 +0300

    up

--- sehir.js
+++ sehir.js
@@ -2,8 +2,8 @@ const url='https://api.openweathermap.org/data/2.5/'
 const key='5419f3be03caa929488a3294fb9ab717'
 
 
-const getResut=()=>{
-    let query=`${url}weather?q=${Trabzon}&appid=${key}&units=metric&lang=tr`
+const getResult=()=>{
+    let query=`${url}weather?q=Trabzon&appid=${key}&units=metric&lang=tr`;
     fetch(query)
     .then(weather=>{
         return weather.json()
@@ -18,3 +18,4 @@ const getResut=()=>{
     desc.innerHTML = `Hava ${result.weather[0].description}`;
     
     }
+    getResult();
