e04b1c89e54b5658415aa4f1930125e864c85a43
BAYRAKTAR <m.bayraktar01@hotmail.com>
2022-05-12 20:39:14 +0300

    up

--- iletisim.php
+++ iletisim.php
@@ -15,6 +15,9 @@
 <header>
  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
   <a class="navbar-brand" href="#" style="font-size: 20px;">Mustafa Bayraktar</a>
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
   <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
     <ul class="navbar-nav">
       <li class="nav-item active">
